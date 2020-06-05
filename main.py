b1322647352ae95bc53d25dcdcba2908fd10fe01
Kostia2004 <K.A.Kudriavsev@mail.ru>
2020-06-05 21:46:25 +0300

    initialize bot

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
 from config import token
-import aiogram
+import logging
+from aiogram import Bot, Dispatcher, executor, types
+
+API_TOKEN = token
+
+# Configure logging
+logging.basicConfig(level=logging.INFO)
+
+# Initialize bot and dispatcher
+bot = Bot(token=API_TOKEN)
+dp = Dispatcher(bot)
