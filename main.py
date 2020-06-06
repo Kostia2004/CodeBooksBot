13c3ca3c1392d0f850d88332d50fe270cc45d7d6
Kostia2004 <K.A.Kudriavsev@mail.ru>
2020-06-06 23:54:30 +0300

    starting polling

--- main.py
+++ main.py
@@ -2,11 +2,12 @@ from config import token
 import logging
 from aiogram import Bot, Dispatcher, executor, types
 
-API_TOKEN = token
-
 # Configure logging
 logging.basicConfig(level=logging.INFO)
 
 # Initialize bot and dispatcher
-bot = Bot(token=API_TOKEN)
+bot = Bot(token=token)
 dp = Dispatcher(bot)
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
