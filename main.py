8a52bf20bcc9dc5a7faf8ccdb22ab2e042612d9f
Kostia2004 <K.A.Kudriavsev@mail.ru>
2020-06-06 23:57:38 +0300

    dded sending welcome message

--- main.py
+++ main.py
@@ -9,5 +9,9 @@ logging.basicConfig(level=logging.INFO)
 bot = Bot(token=token)
 dp = Dispatcher(bot)
 
+@dp.message_handler(commands=['start'])
+async def send_welcome(message: types.Message):
+    await message.reply("Hi!\nI'm PrrogBooksBot!")
+
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)
