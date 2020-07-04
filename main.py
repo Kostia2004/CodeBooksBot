778f8700e0ee2b5fe2e422db720bffd5c0732f83
Kostia2004 <K.A.Kudriavsev@mail.ru>
2020-07-04 05:08:52 +0300

    start menu

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import logging
 from aiogram import Bot, Dispatcher, executor, types
 
 # Configure logging
-logging.basicConfig(level=logging.INFO)
+logging.basicConfig(level=logging.NOTSET)
 
 # Initialize bot and dispatcher
 bot = Bot(token=token)
@@ -11,7 +11,13 @@ dp = Dispatcher(bot)
 
 @dp.message_handler(commands=['start'])
 async def send_welcome(message: types.Message):
-    await message.reply("Hi!\nI'm PrrogBooksBot!")
+    
+    keyboard = types.InlineKeyboardMarkup()
+    language_button = types.InlineKeyboardButton(text='По языку', callback_data = "/language")
+    topic_button = types.InlineKeyboardButton(text="По теме", callback_data = "/topic")
+    keyboard.add(language_button, topic_button)
+    await message.reply("Привет!\nЯ PrrogBooksBot!\nВыберите, как Вам будет удобнее ориентироваться", reply_markup=keyboard)
 
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)
+    
