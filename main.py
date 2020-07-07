35a9dadcf6f688316d5da80233ef4d8d26ebe41b
Kostia2004 <K.A.Kudriavsev@mail.ru>
2020-07-07 03:53:18 +0300

    changed API module, added topic selection

--- main.py
+++ main.py
@@ -1,23 +1,70 @@
 from config import token
 import logging
-from aiogram import Bot, Dispatcher, executor, types
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup
+from telegram.ext import Updater, CommandHandler, CallbackQueryHandler
+import telegram
 
 # Configure logging
 logging.basicConfig(level=logging.NOTSET)
 
 # Initialize bot and dispatcher
-bot = Bot(token=token)
-dp = Dispatcher(bot)
+updater = Updater(token=token, use_context=True)
+dp = updater.dispatcher
 
-@dp.message_handler(commands=['start'])
-async def send_welcome(message: types.Message):
+def start(update, context):
+    keyboard = [[InlineKeyboardButton('По языку', callback_data = "/language"),
+                 InlineKeyboardButton("По теме", callback_data = "/topic")]]
+    reply_markup = InlineKeyboardMarkup(keyboard)
+    context.bot.send_message(chat_id=update.effective_chat.id, text="Привет!\nЯ PrrogBooksBot!\nВыберите, как Вам будет удобнее ориентироваться", reply_markup=reply_markup)
+
+def button(update, context): 
+    query = update.callback_query
+
+    # CallbackQueries need to be answered, even if no notification to the user is needed
+    # Some clients may have trouble otherwise. See https://core.telegram.org/bots/api#callbackquery
+    query.answer()
+    print(query.answer())
+    print("{}".format(query.data))
+    if ("{}".format(query.data)=='/topic'):
+        keyboard = [[InlineKeyboardButton(text='Data Sciense', callback_data = "/DS"),
+                     InlineKeyboardButton(text="Bots", callback_data = "/Bots"),
+                     InlineKeyboardButton(text="Frontend", callback_data = "/frontend")],
+                    [InlineKeyboardButton(text="Backend", callback_data = "/backend"),
+                     InlineKeyboardButton(text="Blockchain", callback_data = "/blockchain"),
+                     InlineKeyboardButton(text="Cryptography", callback_data = "/crypto")],
+                    [InlineKeyboardButton(text="IOT", callback_data = "/iot"),
+                     InlineKeyboardButton(text="Mobile dev", callback_data = "/mobile"),
+                     InlineKeyboardButton(text="Game dev", callback_data = "/games")]]
+
+        reply_markup = InlineKeyboardMarkup(keyboard)
+        
+        context.bot.send_message(chat_id=update.effective_chat.id, text = "Выберите тему", reply_markup=reply_markup)
     
-    keyboard = types.InlineKeyboardMarkup()
-    language_button = types.InlineKeyboardButton(text='По языку', callback_data = "/language")
-    topic_button = types.InlineKeyboardButton(text="По теме", callback_data = "/topic")
-    keyboard.add(language_button, topic_button)
-    await message.reply("Привет!\nЯ PrrogBooksBot!\nВыберите, как Вам будет удобнее ориентироваться", reply_markup=keyboard)
+
+
+def help_command(update, context):
+    update.message.reply_text("Use /start to test this bot.")
+
+
+
+def main():
+    # Create the Updater and pass it your bot's token.
+    # Make sure to set use_context=True to use the new context based callbacks
+    # Post version 12 this will no longer be necessary
+    updater = Updater(token, use_context=True)
+
+    updater.dispatcher.add_handler(CommandHandler('start', start))
+    updater.dispatcher.add_handler(CallbackQueryHandler(button))
+    updater.dispatcher.add_handler(CommandHandler('help', help_command))
+
+    # Start the Bot
+    updater.start_polling()
+
+    # Run the bot until the user presses Ctrl-C or the process receives SIGINT,
+    # SIGTERM or SIGABRT
+    updater.idle()
+
 
 if __name__ == '__main__':
-    executor.start_polling(dp, skip_updates=True)
+    main()
     
